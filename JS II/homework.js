0b310b7654b26d3eaf1880199cec62fe152e2a49
Lauty <38353324+lauty95@users.noreply.github.com>
2022-06-23 16:47:37 -0300

    asd xd

--- JS II/homework.js
+++ JS II/homework.js
@@ -10,7 +10,7 @@ function obtenerMayor(x, y) {
 function estaEnRango(numero) {
     // Devuelve "true" si "numero" es menor que 50 y mayor que 20
     // De lo contrario, devuelve "false"
-    // Tu código:
+    // Tu código:    
 }
 
 function esEntero(numero) {
